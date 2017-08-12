e2f63ccaed69a1713b4973f05eef4097df898697
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-08-12 04:19:43 -0400

    complete I think

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,16 @@
 // Animal Array
 
    var animals = [];
-
-      animals[0] = {name: "Crocodile", animalslist: '<img src=></img>'};
-      animals[1] = {name: "Otter", animalslist: '<img src=></img>'};
-      animals[2] = {name: "Giraffe", animalslist: '<img src=></img>'};
-      animals[3] = {name: "Chimpanzee", animalslist: '<img src=></img>'};
-      animals[4] = {name: "Lion", animalslist: '<img src=></img>'};
-      animals[4] = {name: "Tiger", animalslist:'<img src=></img>'};
-      animals[5] = {name: "Panda", animalslist: '<img src="></img>'};
-      animals[6] = {name: "Gorilla", animalslist: '<img src=></img>'};  
-      animals[7] = {name: "Flamingo", animalslist: '<img src=></img>'}; 
-      animals[8] = {name: "Orangutan", animalslist: '<img src=></img>'};
+      animals[0] = {name: "crocodile", animalslist: '<img src="assets/images/croc.jpg" width="250"></img>'};
+      animals[1] = {name: "otter", animalslist: '<img src="assets/images/otter.jpg" width="250"></img>'};
+      animals[2] = {name: "giraffe", animalslist: '<img src="assets/images/giraffe.jpg" width="250"></img>'};
+      animals[3] = {name: "chimpanzee", animalslist: '<img src="assets/images/chimp.jpg" width="250"></img>'};
+      animals[4] = {name: "lion", animalslist: '<img src="assets/images/lion.jpg" width="250"></img>'};
+      animals[4] = {name: "tiger", animalslist:'<img src="assets/images/tiger.jpg" width="250"></img>'};
+      animals[5] = {name: "panda", animalslist: '<img src="assets/images/panda.jpg" width="250"></img>'};
+      animals[6] = {name: "gorilla", animalslist: '<img src="assets/images/gorilla.jpg" width="250"></img>'};  
+      animals[7] = {name: "flamingo", animalslist: '<img src="assets/images/flamingo.jpg" width="250"></img>'}; 
+      animals[8] = {name: "orangutan", animalslist: '<img src="assets/images/orangutan.jpg" width="250"></img>'};
 
 
  // Variables
@@ -23,7 +22,7 @@
     var lettersGuessed;
     var triesRemaining;
 
-
+    var input;
     var randomAnimal;
     var pickedAnimal;
     var placeholder;
